7922e978f312a47c4d245e3892bf2f2b64b377ba
dwiptrdo <alfredoputradwi@gmail.com>
2024-01-21 10:45:35 +0700

    Update

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Halo')
+import requests
+from pyquery import PyQuery
+
+response = requests.get('https://www.cnbcindonesia.com/news/indeks/3/1')
+print(response)
+
+html = PyQuery(response.text)
+
+link = html.find('ul[class="list media_rows middle thumb terbaru gtm_indeks_feed"] >li')
+print(link.find('a').attr('href'))
+
+for url in link:
+    print(PyQuery(url).find('a').attr('href'))
+
